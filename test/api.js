5641090143d1db3d9a48d2cd5e290dae7c72a4ab
jetiny <86287344@qq.com>
2016-11-22 17:17:24 +0800

    fixed test

--- test/api.js
+++ test/api.js
@@ -3,10 +3,10 @@ import {Api} from '../'
 import {isObject, isFunction} from 'sav-util'
 
 test('Api#api', ava => {
-  ava.ture(isFunction(Api))
+  ava.true(isFunction(Api))
   let api = new Api()
-  ava.ture(isObject(api))
-  ava.ture(isFunction(api.declare))
-  ava.ture(isFunction(api.setBaseUrl))
-  ava.ture(isFunction(api.setAjax))
+  ava.true(isObject(api))
+  ava.true(isFunction(api.declare))
+  ava.true(isFunction(api.setBaseUrl))
+  ava.true(isFunction(api.setAjax))
 })
